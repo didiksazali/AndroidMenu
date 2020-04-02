5fc414cc5cdef110f0ec1c2546ee5b28edfb38a1
didiksazali <didiksazali@gmail.com>
2020-04-02 08:45:20 +0700

    Initial commit

---
+++ app/src/main/java/apk/menu/android/didiksazali/menu/KulkasActivity.java
@@ -0,0 +1,13 @@
+package apk.menu.android.didiksazali.menu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KulkasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kulkas);
+    }
+}
